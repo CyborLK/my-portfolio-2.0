e7674e5a91f4f8f6dd42a573197f33b9bad5199c
NadunDaluwatta <nadundaluwatta26@gmail.com>
2024-12-12 10:03:11 +0530

    update contect data

--- my-portfolio/components/Connect/ConnectData.jsx
+++ my-portfolio/components/Connect/ConnectData.jsx
@@ -4,8 +4,8 @@ export const connectData = {
         title2: "Get in touch",
         title3: "Let's Connect",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
-        phone: "+976 12 34 9999",
-        email: "flatheme@gmail.com",
-        address: "121 King St, Melbourne VIC 3000",
+        phone: "+94 76 22 13 088",
+        email: "nadunsd@gmail.com",
+        address: "Matale, Sri Lanka",
     }
 };
