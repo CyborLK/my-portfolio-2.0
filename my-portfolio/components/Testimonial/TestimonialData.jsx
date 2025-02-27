b00a636b87ca520e19189dbd865d216a9bb8a89f
Your Name <youremail@example.com>
2025-02-27 22:55:40 +0530

    lanka review update

--- my-portfolio/components/Testimonial/TestimonialData.jsx
+++ my-portfolio/components/Testimonial/TestimonialData.jsx
@@ -1,5 +1,5 @@
-import TestimonialAvatar from '@/public/images/testimonial-avatar.jpg';
 import TestimonialAvatar1 from '@/public/images/testimonial-avatar-madushan.jpg';
+import TestimonialAvatar2 from '@/public/images/testimonial-avatar-lanka.jpg';
 
 export const testimonialData = {
     mainData: {
@@ -10,8 +10,14 @@ export const testimonialData = {
         {
             description: 'I highly recommend Nadun as a talented Graphic Designer and Social Media Manager. Their creativity, attention to detail, and ability to craft engaging content make them an asset to any brand. If you are looking for someone to elevate your digital presence, Nadun is the right choice!',
             name: 'Madushan Jayasinghe',
-            jobTitle: 'A/L Tutor',
+            jobTitle: 'A/L ICT Tutor',
             avatar: TestimonialAvatar1,
+        },
+        {
+            description: 'I highly recommend Nadun as a talented and creative Graphic Designer. His exceptional design skills, attention to detail, and ability to bring ideas to life make him a valuable asset to any brand. Whether it s logo design, social media graphics, or branding materials, he always delivers outstanding work that exceeds expectations. If you are looking for a skilled designer to elevate your brand’s visual identity, Nadun is the perfect choice!',
+            name: 'Nayana Lanka Disanayaka',
+            jobTitle: 'A/L SFT Tutor',
+            avatar: TestimonialAvatar2,
         },
         
     ]
